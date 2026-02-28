0a8852434e9237b0fa2bba2af7625b18006a9cb0
hakimcolor <hakimcolor777@gmail.com>
2026-02-28 13:10:36 +0600

    i add now singin and singup page and fix the routing system

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,20 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from 'react-router';
+import SignIn from '../Pages/Singin';
+import SingUP from '../Pages/SingUp';
+import Root from '../Root/Root';
 
- export const router = createBrowserRouter([
-  {path:'/',element:<h1>Hello workd</h1>}
-])
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <SignIn />,
+      }, {
+        path: '/signup',
+        element:<SingUP/>
+      }
+    ],
+  },
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router'
-import { router } from './Router/Router.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
+import { RouterProvider } from 'react-router';
+import { router } from './Router/Router.jsx';
+import { ToastContainer } from 'react-toastify';
+import AuthProvider from './Context/AuthProvider.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={ router} />
-  </StrictMode>,
-)
+    <AuthProvider>
+      {' '}
+      <RouterProvider router={router} />
+    </AuthProvider>
+
+    <ToastContainer />
+  </StrictMode>
+);
